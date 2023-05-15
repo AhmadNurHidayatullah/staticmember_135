f1086ce7e39338c88e2ce92756b11e1e182d98ff
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-05-15 11:26:20 +0700

    manambahkan class mahasiswa

--- staticmember_135/staticmember_135.cpp
+++ staticmember_135/staticmember_135.cpp
@@ -1,8 +1,10 @@
-// staticmember_135.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
+class mahasiswa {
+
+};
 int main()
 {
     std::cout << "Hello World!\n";
