5f95a50eaadb012809850630de988e0c78d11bce
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-05-15 16:38:54 +0700

    menambahkan fungsi

--- konsruktor/konsruktor.cpp
+++ konsruktor/konsruktor.cpp
@@ -13,6 +13,24 @@ public:
     mahasiswa(int iNim, string iNama);
     void cetak();
 };
+
+mahasiswa::mahasiswa() {
+    nim = 0;
+    nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+    nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+    nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+    nim = iNim;
+    nama = iNama;
+}
 int main()
 {
     std::cout << "Hello World!\n";
