39b1951bc0394d25505a53efa2a1dd08742bfc86
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-05-15 11:28:41 +0700

    menambahkan fungsi class mahasiswa

--- staticmember_135/staticmember_135.cpp
+++ staticmember_135/staticmember_135.cpp
@@ -3,8 +3,16 @@
 using namespace std;
 
 class mahasiswa {
+public:
+    static int nim;
+    int id;
+    string nama;
 
+    void setId();
+    void printAll();
+    mahasiswa(string pnama) :nama(pnama) { setId(); }
 };
+
 int main()
 {
     std::cout << "Hello World!\n";
